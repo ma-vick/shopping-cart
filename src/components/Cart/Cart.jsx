3584e9ca56b8425c5cceda20ec0d452db6e55b6c
Victória Almeida <mariavictoriafalmeida@gmail.com>
2023-08-27 19:59:13 -0300

    add btn remove cart items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,26 @@
-import { useContext } from 'react';
-import CartItem from '../CartItem/CartItem';
-import './Cart.css';
-import AppContext from '../../context/AppContext';
-import formatCurrency from '../../utils/formatCurrency';
+import { BsCartXFill } from 'react-icons/bs';
+import { useContext } from "react";
+import CartItem from "../CartItem/CartItem";
+import "./Cart.css";
+import AppContext from "../../context/AppContext";
+import formatCurrency from "../../utils/formatCurrency";
 
 function Cart() {
-  const { cartItems, isCartVisible } = useContext(AppContext);
+  const { cartItems, setCartItems, isCartVisible } = useContext(AppContext);
 
   const totalPrice = cartItems.reduce((acc, item) => item.price + acc, 0);
 
   return (
-    <section className={`cart ${isCartVisible ? 'cart--active' : ''}`} >
+    <section className={`cart ${isCartVisible ? "cart--active" : ""}`}>
       <div className="cart-items">
-        { cartItems.map((cartItem) => <CartItem key={cartItem.id} data={cartItem} />) }
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} data={cartItem} />
+        ))}
       </div>
-      <div className="cart-resume">{ formatCurrency(totalPrice, 'BRL') }</div>
+      <span className="cart-resume-content">
+        <div className="cart-resume">{formatCurrency(totalPrice, "BRL")}</div>
+        <button className="btn-remove-all" onClick={ () => setCartItems([]) }><BsCartXFill /></button>
+      </span>
     </section>
   );
 }
